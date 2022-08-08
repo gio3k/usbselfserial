68b84b3e63270c6b5b82ca23e62ae8846a0d8c30
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-08-08 18:08:29 +0800

    core(debug): add debug strings until working

--- driver/handler.py
+++ driver/handler.py
@@ -193,14 +193,14 @@ class CommonUSBDeviceHandler(BaseUSBDeviceHandler):
             while buf is not None:
                 buf = os.read(self.__pty_fd, 32)
                 self._handle.bulkWrite(self._write_endpoint, buf, 5000)
-                #print("from pty to printer >", buf)
+                print("from pty to printer >", buf)
         except os.error as err:
             if err.errno not in (errno.EAGAIN, errno.EWOULDBLOCK):
                 raise err
 
     def __read_callback(self, transfer: USBTransfer) -> None:
         buf = transfer.getBuffer()[:transfer.getActualLength()]
-        #print("from printer to pty >", buf)
+        print("from printer to pty >", buf)
         os.write(self.__pty_fd, buf)
         self.__transfer.submit()
 
