8780edf16ff40da22bae16cc67461e99b0fb1894
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-09-02 16:46:29 +0800

    creator: rewrite

--- creator.hpp
+++ creator.hpp
@@ -13,21 +13,17 @@
  *     * (by the time you read this it could have a different name!)
  * - 2022
  */
-#include "types.hpp"
-#include <chrono>
-#include <cstdio>
-#include <libusb-1.0/libusb.h>
+#include "driver/base.hpp"
+#include "output/base.hpp"
 #include <stdio.h>
-#include <thread>
+#include <libusb-1.0/libusb.h>
+#include <type_traits>
 
 namespace usbselfserial {
 
-template <typename DeviceT, typename OutputT> struct CreatorInstanceData {
-    libusb_device_handle* device_handle = 0x0;
-    DeviceT* device_instance = 0x0;
-    OutputT* output_instance = 0x0;
-    void* output_config_data = 0x0;
+struct CreatorConnectionData {
     bool connected = false;
+    libusb_device_handle* device_handle = NULL;
 };
 
 /**
@@ -35,25 +31,34 @@ template <typename DeviceT, typename OutputT> struct CreatorInstanceData {
  * @tparam DeviceT Device / driver class to use
  * @tparam OutputT Output class to use
  */
-template <typename DeviceT, typename OutputT> class Creator {
+template <class DeviceT, class OutputT> class Creator {
+    static_assert(std::is_base_of<driver::BaseDevice, DeviceT>::value,
+                  "DeviceT must inherit BaseDevice.");
+    static_assert(std::is_base_of<output::BaseOutput, OutputT>::value,
+                  "OutputT must inherit BaseOutput.");
+
+protected:
+    driver::BaseDevice* device = 0x0;
+    output::BaseOutput* output = 0x0;
+    CreatorConnectionData connection;
+
 private:
-    CreatorInstanceData<DeviceT, OutputT> data;
     libusb_hotplug_callback_handle handle_callback;
 
     static int hotplug_callback(struct libusb_context* ctx,
                                 struct libusb_device* dev,
                                 libusb_hotplug_event event, void* user_data) {
-        CreatorInstanceData<DeviceT, OutputT>* data =
-            (CreatorInstanceData<DeviceT, OutputT>*)user_data;
-
+        CreatorConnectionData* connection = (CreatorConnectionData*)user_data;
         if (event == LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED) {
-            int ret = libusb_open(dev, &data->device_handle);
+            int ret = libusb_open(dev, &connection->device_handle);
             if (ret != 0)
                 printf("Failed to open connected device. code: %i (%s)\n", ret,
                        libusb_error_name(ret));
-            data->connected = true;
+            connection->connected = true;
         } else if (event == LIBUSB_HOTPLUG_EVENT_DEVICE_LEFT) {
-            data->connected = false;
+            printf("device disconnect\n");
+            connection->connected = false;
+            connection->device_handle = NULL;
         }
 
         return 0;
@@ -67,68 +72,67 @@ public:
             LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED |
                 LIBUSB_HOTPLUG_EVENT_DEVICE_LEFT,
             0, vendor_id, product_id, LIBUSB_HOTPLUG_MATCH_ANY,
-            hotplug_callback, &data, &handle_callback);
+            hotplug_callback, &connection, &handle_callback);
 
-        if (ret != 0)
+        if (ret < 0)
             printf("Failed to create hotplug callback. code: %i (%s)\n", ret,
                    libusb_error_name(ret));
 
-        // Set config data
-        data.output_config_data = output_config;
+        // Create output
+        printf("Creating output...\n");
+        output = new OutputT(output_config);
 
         // Try to get device handle
-        data.device_handle =
+        connection.device_handle =
             libusb_open_device_with_vid_pid(NULL, vendor_id, product_id);
-        if (data.device_handle != 0x0)
-            data.connected = true;
+        if (connection.device_handle != 0x0)
+            connection.connected = true;
+        else if (ret < 0)
+            throw "Failed to set up hotplug and device not found.";
     }
 
-    ~Creator() { libusb_hotplug_deregister_callback(NULL, handle_callback); }
+    ~Creator() {
+        // todo: actual clean up
+        libusb_hotplug_deregister_callback(NULL, handle_callback);
+    }
 
     bool Create() {
-        if (!data.connected) {
-            if (data.output_instance == 0x0 && data.device_instance == 0x0)
+        if (!connection.connected) {
+            if (device == NULL)
                 return false;
 
-            // if output instance exists...
-            if (data.output_instance != 0x0) {
-                // request completion
-                data.output_instance->TryFinalize();
+            // If device instance still exists but the connection is gone...
+            // try to finalize output
+            output->TryFinalize();
 
-                // if it's already finished...
-                if (data.output_instance->HasFinished()) {
-                    // delete the instance
-                    delete data.output_instance;
-                    data.output_instance = 0x0;
-                }
+            // If output hasn't finished...
+            if (!output->HasFinished())
+                return false; // return while we wait.
+
+            if (output->HasLinkedDevice()) {
+                // Unlink the device if required
+                output->UnlinkDevice();
+            } else {
+                delete device;
+                device = NULL;
             }
-
-            // if device instance exists but output instance doesn't...
-            if (data.output_instance == 0x0 && data.device_instance != 0x0) {
-                // delete the instance
-                delete data.device_instance;
-                data.device_instance = 0x0;
-            }
-
+            
             return false;
         }
 
-        bool out = false;
-        if (data.device_instance == 0x0) {
-            data.device_instance = new DeviceT(data.device_handle);
-            out = true;
-        }
-        if (data.output_instance == 0x0) {
-            data.output_instance =
-                new OutputT(*data.device_instance, data.output_config_data);
-            out = true;
+        if (device == NULL) {
+            // Create new device
+            printf("Creating new device instance (usb_handle %p)!\n", connection.device_handle);
+            device = new DeviceT(connection.device_handle);
+            output->LinkDevice(device);
+            return true;
         }
 
-        return out;
+        return false;
     }
 
-    DeviceT* GetDevice() { return data.device_instance; }
-    OutputT* GetOutput() { return data.output_instance; }
+    DeviceT* GetDevice() { return (DeviceT*)device; }
+    OutputT* GetOutput() { return (OutputT*)output; }
 };
 
 } // namespace usbselfserial
