f3d8d55dd774db66e5410bc0893dfe302de4ad55
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-09-04 04:02:08 +0800

    v3: include file

---
+++ uss/uss.hpp
@@ -0,0 +1,29 @@
+/**
+ * usbselfserial by lotuspar (https://github.com/lotuspar)
+ *
+ * Inspired / based on:
+ *     the usb-serial-for-android project made in Java,
+ *         * which is copyright 2011-2013 Google Inc., 2013 Mike Wakerly
+ *         * https://github.com/mik3y/usb-serial-for-android
+ *     the Linux serial port drivers,
+ *         * https://github.com/torvalds/linux/tree/master/drivers/usb/serial
+ *     and the FreeBSD serial port drivers
+ *         * https://github.com/freebsd/freebsd-src/tree/main/sys/dev/usb/serial
+ * Some parts rewritten in C++ for usbselfserial!
+ *     * (by the time you read this it could have a different name!)
+ * - 2022
+ */
+/**
+ * This is just a file of common includes.
+ */
+
+// Drivers
+#include "drivers/cdcacm/cdcacm.hpp"
+#include "drivers/ch34x/ch34x.hpp"
+
+// Outputs
+#include "outputs/pty/pty.hpp"
+
+// Controllers
+#include "controllers/basic.hpp"
+#include "controllers/hotpluggable.hpp"
