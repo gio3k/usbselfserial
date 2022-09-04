ddde1b794343de1a9f49820268001a39ed883e71
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-09-04 20:28:57 +0800

    update example!
    uses a thread for writing now!

--- example.cpp
+++ example.cpp
@@ -1,9 +1,13 @@
+#include "uss/drivers/cdcacm/cdcacm.hpp"
 #include "uss/drivers/ch34x/ch34x.hpp"
 #include "uss/uss.hpp"
+#include <cstdio>
+#include <thread>
 
 using namespace uss;
 
 int main(int argc, char** argv) {
+    bool active = true;
     libusb_init(NULL);
 
     // Create a driver
@@ -14,6 +18,10 @@ int main(int argc, char** argv) {
     // This outputs to a virtual serial port / PTY at /tmp/uss0
     uss::output::pty::PtyOutput output(NULL, "/tmp/uss0", true);
 
+    // Set output transfer completion callback
+    output.SetTransferCompletionCallback(
+        [&output](int result) { output.RemoveDevice(); });
+
     // Create a device
     // This uses the device 1a86:7523 and supports hotplug
     uss::ctl::Hotpluggable ctl(
@@ -22,7 +30,7 @@ int main(int argc, char** argv) {
             output.SetDevice(device);
         }, // Device connected event
         [&output](uss::ctl::Hotpluggable* device) {
-            output.RemoveDevice();
+            output.EndTransfers();
         }); // Device disconnected event
 
     // Set device baud rate (250000)
@@ -31,20 +39,24 @@ int main(int argc, char** argv) {
     // Set the device driver to the CH34x one from before
     ctl.SetDriver(&driver);
 
-    struct timeval tv = {0L, 0L};
-    while (true) {
+    std::thread output_thread([&output, &active]() {
+        while (active) {
+            output.HandleEvents();
+        }
+    });
+
+    struct timeval tv = {1L, 0L};
+    while (active) {
         // For each loop iteration...
         try {
             // Handle LibUSB events
-            int ret = libusb_handle_events_timeout(NULL, &tv);
+            int ret = libusb_handle_events_timeout_completed(NULL, &tv, NULL);
             if (ret < 0)
                 goto clean;
 
             // Update device
             ctl.Update();
 
-            // Update output
-            output.Update();
         } catch (const char* error) {
             printf("Error caught during main loop: %s\n", error);
             goto clean;
@@ -52,6 +64,8 @@ int main(int argc, char** argv) {
     }
 
 clean:
+    active = false;
+    output_thread.join();
     libusb_exit(NULL);
     return 2;
 }
